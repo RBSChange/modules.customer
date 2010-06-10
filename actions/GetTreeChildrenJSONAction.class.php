29452bda8fc69d1302142527a3cfd56119ea74ed
Franck STAUFFER <franck.stauffer@rbs.fr>
2010-06-10 11:06:50 +0000

    [FIX] jquery no conflict mode

---
+++ actions/GetTreeChildrenJSONAction.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package modules.customer
+ */
+class customer_GetTreeChildrenJSONAction extends generic_GetTreeChildrenJSONAction
+{
+	/**
+	 * @param f_persistentdocument_PersistentDocument $document
+	 * @param string[] $subModelNames
+	 * @param string $propertyName
+	 * @return array<f_persistentdocument_PersistentDocument>
+	 */
+	protected function getVirtualChildren($document, $subModelNames, $propertyName)
+	{
+		if ($document instanceof customer_persistentdocument_dynamiccustomergroup)
+		{
+			$queryIntersection = f_persistentdocument_DocumentFilterService::getInstance()->getQueryIntersectionFromJson($document->getQuery());
+			$totalCount = 0;
+			$result = $queryIntersection->findAtOffset($this->getStartIndex(), $this->getPageSize(), $totalCount);
+			$this->setTotal($totalCount);		
+			return $result;
+		}
+		else
+		{
+			return parent::getVirtualChildren($document, $subModelNames, $propertyName);
+		}
+	}
+}
