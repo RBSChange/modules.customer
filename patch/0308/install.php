f0617934c84b81f1b7b0c766a765738915a8b9ab
Gaël PORT <gael.port@rbs.fr>
2010-08-12 11:32:04 +0200

    [NEW] Added lastAbandonedOrderDate property on customers.

---
+++ patch/0308/install.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * customer_patch_0308
+ * @package modules.customer
+ */
+class customer_patch_0308 extends patch_BasePatch
+{
+	/**
+	 * Entry point of the patch execution.
+	 */
+	public function execute()
+	{
+		$newPath = f_util_FileUtils::buildWebeditPath('modules/customer/persistentdocument/customer.xml');
+		$newModel = generator_PersistentModel::loadModelFromString(f_util_FileUtils::read($newPath), 'customer', 'customer');
+		$newProp = $newModel->getPropertyByName('lastAbandonedOrderDate');
+		f_persistentdocument_PersistentProvider::getInstance()->addProperty('customer', 'customer', $newProp);
+	}
+	
+	/**
+	 * @return String
+	 */
+	protected final function getModuleName()
+	{
+		return 'customer';
+	}
+	
+	/**
+	 * @return String
+	 */
+	protected final function getNumber()
+	{
+		return '0308';
+	}
+}
