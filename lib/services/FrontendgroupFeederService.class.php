a917ab15913622c0a4d3d64424452a47c3c5f963
Gaël PORT <gael.port@rbs.fr>
2010-08-30 15:16:22 +0200

    [NEW] Enable to create dynamic frontend group from a customer group.

---
+++ lib/services/FrontendgroupFeederService.class.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author intportg
+ * @package modules.customer
+ */
+class customer_FrontendgroupFeederService extends users_FrontendgroupFeederBaseService
+{
+	/**
+	 * @var customer_FrontendgroupFeederService
+	 */
+	private static $instance;
+
+	/**
+	 * @return customer_FrontendgroupFeederService
+	 */
+	public static function getInstance()
+	{
+		if (self::$instance === null)
+		{
+			self::$instance = self::getServiceClassInstance(get_class());
+		}
+		return self::$instance;
+	}	
+
+	/**
+	 * @param users_persistentdocument_dynamicfrontendgroup $group
+	 */
+	public function getUserIds($group)
+	{
+		$docId = $group->getParameter('referenceId');
+		if ($docId !== null)
+		{
+			$doc = DocumentHelper::getDocumentInstance($docId, 'modules_customer/customergroup');
+			$customerIds = $doc->getDocumentService()->getMemberIds($doc);
+			$query = users_WebsitefrontenduserService::getInstance()->createQuery()->add(Restrictions::in('customer.id', $customerIds));
+			return $query->setProjection(Projections::property('id'))->findColumn('id');
+		}
+		return array();
+	}
+}
