d1291fd8b117e57c989f102de3ca4dbaa2c6af0e
Gaël PORT <gael.port@rbs.fr>
2010-10-15 09:42:48 +0200

    [NEW] Handle "paid", "uncancelled" and "all" order categories in OrderCountCustomerFilter.

--- persistentdocument/filters/OrderCountCustomerFilter.php
+++ persistentdocument/filters/OrderCountCustomerFilter.php
@@ -6,6 +6,11 @@ class customer_OrderCountCustomerFilter extends customer_OrderFilterBase
 		$info = new BeanPropertyInfoImpl('count', 'Integer');
 		$parameter = f_persistentdocument_DocumentFilterRestrictionParameter::getNewHavingInstance($info);
 		$this->setParameter('count', $parameter);
+
+		$info = new BeanPropertyInfoImpl('status', 'String');
+		$info->setListId('modules_order/statusesforordercountfilter');
+		$parameter = new f_persistentdocument_DocumentFilterValueParameter($info);
+		$this->setParameter('status', $parameter);
 	}
 	
 	/**
@@ -22,9 +27,27 @@ class customer_OrderCountCustomerFilter extends customer_OrderFilterBase
 	public function getQuery()
 	{
 		$query = customer_CustomerService::getInstance()->createQuery();
-		$subQuery = $query->createLeftCriteria('paidByCustomerId', 'modules_order/bill');
-		$subQuery->setProjection(Projections::distinctCount('order', 'count'));
-		$query->having($this->getParameter('count')->getValueForQuery());
+		switch ($this->getParameter('status')->getValueForQuery())
+		{
+			case 'paid':
+				$subQuery = $query->createLeftCriteria('paidByCustomerId', 'modules_order/bill');
+				$subQuery->setProjection(Projections::distinctCount('order', 'count'));
+				$query->having($this->getParameter('count')->getValueForQuery());
+				break;
+			
+			case 'uncancelled':
+				$subQuery = $query->createLeftCriteria('customer', 'modules_order/order');
+				$subQuery->add(Restrictions::orExp(Restrictions::ne('orderStatus', order_OrderService::CANCELED), Restrictions::isNull('orderStatus')));
+				$subQuery->setProjection(Projections::rowCount('count'));
+				$query->having($this->getParameter('count')->getValueForQuery());
+				break;
+			
+			case 'all':
+				$subQuery = $query->createLeftCriteria('customer', 'modules_order/order');
+				$subQuery->setProjection(Projections::rowCount('count'));
+				$query->having($this->getParameter('count')->getValueForQuery());
+				break;
+		}
 		return $query;
 	}
 }
