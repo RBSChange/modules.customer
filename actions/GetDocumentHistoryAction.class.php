1382cd49dea7d1226e85517367d0b1d4bb2ae358
Gaël PORT <gael.port@rbs.fr>
2011-01-17 16:58:24 +0100

    [REFACTOR] Use the new sortLog method.

--- actions/GetDocumentHistoryAction.class.php
+++ actions/GetDocumentHistoryAction.class.php
@@ -14,22 +14,8 @@ class customer_GetDocumentHistoryAction extends generic_GetDocumentHistoryAction
 		if ($document instanceof customer_persistentdocument_customer)
 		{
 			$logs = array_merge($logs, parent::getLogs(null, $document->getUser()));
-			usort($logs, array($this, 'compareLogEntries'));
+			$logs = $this->sortLogs($logs);
 		}
 		return $logs;
-	}
-	
-	/**
-	 * @param Array $a
-	 * @param Array $b
-	 * @return Integer
-	 */
-	public function compareLogEntries($a, $b)
-	{
-		if ($a['entry_date'] == $b['entry_date'])
-		{
-			return 0; 
-		}
-		return (($a['entry_date'] > $b['entry_date']) ? -1 : 1);
 	}
 }
