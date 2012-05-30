3de1f77ea3da1e4313329fe8820e6f8c4af149ce
Eric HAUSWALD <eric.hauswald@rbs.fr>
2012-05-30 11:49:06 +0200

    [REFACTOR] Convert f_util_System::execHTTPScript to f_util_System::execScript

--- actions/ExportDynGroupAction.class.php
+++ actions/ExportDynGroupAction.class.php
@@ -40,7 +40,7 @@ class customer_ExportDynGroupAction extends f_action_BaseAction
 	
 		foreach (array_chunk($ids, 250) as $chunk)
 		{
-			$result = f_util_System::execHTTPScript($batchPath, array_merge(array($tmpFileName), $chunk));
+			$result = f_util_System::execScript($batchPath, array_merge(array($tmpFileName), $chunk));
 			if ($result !== 'OK')
 			{
 				Framework::error(__METHOD__ . " " . $result);
