870e927019f9a801d35040695320dba944b8e977
Gaël PORT <gael.port@rbs.fr>
2012-09-19 17:39:34 +0200

    [DELETE] Delete old samples.

--- setup/initData.php
+++ setup/initData.php
@@ -1,38 +1,29 @@
 <?php
 class customer_Setup extends object_InitDataSetup
 {
+	public function install()
+	{
+		$this->executeModuleScript('init.xml');
+
+		$mbs = uixul_ModuleBindingService::getInstance();
+		$mbs->addImportInPerspective('catalog', 'customer', 'catalog.perspective');
+		$mbs->addImportInActions('catalog', 'customer', 'catalog.actions');
+		$result = $mbs->addImportform('catalog', 'modules_customer/voucherhandler');
+		if ($result['action'] == 'create')
+		{
+			uixul_DocumentEditorService::getInstance()->compileEditorsConfig();
+		}
+
+		change_PermissionService::getInstance()->addImportInRight('catalog', 'customer', 'catalog.rights');
+	}
+	
 	/**
 	 * Returns an array of packages names that are required to install the data.
 	 * Please override this method in the initData.php of your module if needed.
-	 * @return Array<String>
+	 * @return string[]
 	 */
 	public function getRequiredPackages()
 	{
 		return array('modules_users', 'modules_zone');
-	}
-
-	public function install()
-	{
-		try
-		{
-			$scriptReader = import_ScriptReader::getInstance();
-			$scriptReader->executeModuleScript('customer', 'init.xml');
-			
-			$mbs = uixul_ModuleBindingService::getInstance();
-			$mbs->addImportInPerspective('catalog', 'customer', 'catalog.perspective');
-			$mbs->addImportInActions('catalog', 'customer', 'catalog.actions');
-			$result = $mbs->addImportform('catalog', 'modules_customer/voucherhandler');
-			if ($result['action'] == 'create')
-			{
-				uixul_DocumentEditorService::getInstance()->compileEditorsConfig();
-			}
-
-			change_PermissionService::getInstance()->addImportInRight('catalog', 'customer', 'catalog.rights');
-		}
-		catch (Exception $e)
-		{
-			echo "ERROR: " . $e->getMessage() . "\n";
-			Framework::exception($e);
-		}
 	}
 }
